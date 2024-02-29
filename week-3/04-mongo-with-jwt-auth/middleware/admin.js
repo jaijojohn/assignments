e01a18f79f2c434fd493753c7c1346ebca6c34c4
jaijo999 <jaijojohn999@gmail.com>
2024-02-29 17:19:47 +0530

    git test for dev

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -3,5 +3,5 @@ function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
 }
-
+ 
 module.exports = adminMiddleware;
